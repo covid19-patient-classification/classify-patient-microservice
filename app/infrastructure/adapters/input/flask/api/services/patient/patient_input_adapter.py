be211aaf04a04de3a13360816f83e6fad7162285
carloscastillo10 <carloscastillo090916@gmail.com>
2022-09-16 17:18:10 -0500

    :sparkles: Adding random forest adapter

--- app/infrastructure/adapters/input/flask/api/services/patient/patient_input_adapter.py
+++ app/infrastructure/adapters/input/flask/api/services/patient/patient_input_adapter.py
@@ -1,7 +1,12 @@
+from app.infrastructure.adapters.output.randomforest.patient_classification_random_forest_adapter import PatientClassificationRandomForestAdapter
 from app.infrastructure.adapters.input.flask.api.services.patient import blueprint
 from flask import jsonify
+
+patient_classification_random_forest_adapter = PatientClassificationRandomForestAdapter()
 
 
 @blueprint.route('/classify', methods=['GET'])
 def classify():
-    return jsonify({'status_code': 200, 'message': 'Classified'})
+    return jsonify(patient_classification_random_forest_adapter.classify_patient(
+        90, 90, 90, 90, True, True, True, True, True, True, True, True, True, True, True, True
+    ))
