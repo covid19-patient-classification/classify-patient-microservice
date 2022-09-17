4f73545f2faf098ae3276cd3f46db30ffc34ae76
carloscastillo10 <carloscastillo090916@gmail.com>
2022-09-17 18:40:19 -0500

    :hammer: Update patient classification input adapter

--- app/infrastructure/adapters/input/flask/api/services/patient/patient_input_adapter.py
+++ app/infrastructure/adapters/input/flask/api/services/patient/patient_input_adapter.py
@@ -1,12 +1,13 @@
-from app.infrastructure.adapters.output.randomforest.patient_classification_random_forest_adapter import PatientClassificationRandomForestAdapter
+from app.domain.entity.patient import Patient
+from app.infrastructure.adapters.output.random_forest.patient_classification_random_forest_adapter import PatientClassificationRandomForestAdapter
 from app.infrastructure.adapters.input.flask.api.services.patient import blueprint
 from flask import jsonify
-
-patient_classification_random_forest_adapter = PatientClassificationRandomForestAdapter()
 
 
 @blueprint.route('/classify', methods=['GET'])
 def classify():
-    return jsonify(patient_classification_random_forest_adapter.classify_patient(
-        90, 90, 90, 90, True, True, True, True, True, True, True, True, True, True, True, True
-    ))
+    patient = Patient("1100000000", "CXXXXXX", 95, 90, 90, 290, True,
+                      True, True, True, True, True, True, True, True, True, True, True)
+    covid19_severity = PatientClassificationRandomForestAdapter().classify_patient(patient)
+    patient.set_covid19_severity(covid19_severity) # Aquí se puede agregar una nueva funcionalidad en el caso de uso de management
+    return jsonify(patient.__rules__())
