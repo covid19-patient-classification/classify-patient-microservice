08d62d34995fbc85bd960f6449983b706e6c369a
carloscastillo10 <carloscastillo090916@gmail.com>
2022-09-17 18:35:22 -0500

    :heavy_plus_sign: Adding ia model dependencies

---
+++ app/infrastructure/adapters/output/random_forest/patient_classification_random_forest_adapter.py
@@ -0,0 +1,31 @@
+from app.application.ports.output.patient_classification_output_port import PatientClassificationOutputPort
+from app.infrastructure.adapters.output.randomforest.mappers.patient_random_forest_mapper import PatientRandomForestMapper
+import joblib
+
+
+class PatientClassificationRandomForestAdapter(PatientClassificationOutputPort):
+    def __init__(self):
+        self.model_path = 'resources/model.joblib'
+        self.random_forest_model = self.set_up_model(self.model_path)
+
+    def classify_patient(self, patient):
+        patient_dataframe = PatientRandomForestMapper.patient_domain_to_dataframe(patient)
+        print(patient_dataframe)
+        # covid19_severity_prediction = self.predict(self.random_forest_model, patient_dataframe)
+        # return PatientRandomForestMapper.covid19_severity_prediction_to_string(covid19_severity_prediction)
+        return {"code": 200}
+
+    @staticmethod
+    def set_up_model(model_path):
+        return joblib.load('{}'.format(model_path))
+
+    @staticmethod
+    def predict(random_forest_model, patient_dataframe):
+        return random_forest_model.predict(patient_dataframe)
+
+    @staticmethod
+    def train(): pass
+
+    @staticmethod
+    def save(random_forest_model, model_path):
+        joblib.dump(random_forest_model, model_path)
