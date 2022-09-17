8b52dd041d5e09372b9895feba6e753df44f7dd3
carloscastillo10 <carloscastillo090916@gmail.com>
2022-09-17 18:39:59 -0500

    :hammer: Update random forest adapter

--- app/infrastructure/adapters/output/random_forest/patient_classification_random_forest_adapter.py
+++ app/infrastructure/adapters/output/random_forest/patient_classification_random_forest_adapter.py
@@ -1,19 +1,17 @@
 from app.application.ports.output.patient_classification_output_port import PatientClassificationOutputPort
-from app.infrastructure.adapters.output.randomforest.mappers.patient_random_forest_mapper import PatientRandomForestMapper
+from app.infrastructure.adapters.output.random_forest.mappers.patient_random_forest_mapper import PatientRandomForestMapper
 import joblib
 
 
 class PatientClassificationRandomForestAdapter(PatientClassificationOutputPort):
     def __init__(self):
-        self.model_path = 'resources/model.joblib'
+        self.model_path = 'app/infrastructure/adapters/output/random_forest/resources/model.joblib'
         self.random_forest_model = self.set_up_model(self.model_path)
 
     def classify_patient(self, patient):
         patient_dataframe = PatientRandomForestMapper.patient_domain_to_dataframe(patient)
-        print(patient_dataframe)
-        # covid19_severity_prediction = self.predict(self.random_forest_model, patient_dataframe)
-        # return PatientRandomForestMapper.covid19_severity_prediction_to_string(covid19_severity_prediction)
-        return {"code": 200}
+        covid19_severity_prediction = self.predict(self.random_forest_model, patient_dataframe)
+        return PatientRandomForestMapper.covid19_severity_prediction_to_string(covid19_severity_prediction[0])
 
     @staticmethod
     def set_up_model(model_path):
